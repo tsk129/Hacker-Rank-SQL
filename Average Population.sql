ef04e3377920eeb2f167194bd6d4bdb25b7a2459
SAMPATH <59731374+tsk129@users.noreply.github.com>
2022-02-21 22:30:05 +0530

    Create Average Population.sql

---
+++ Average Population.sql
@@ -0,0 +1,7 @@
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+
+Input Format
+
+The CITY table is described as follows:CITY.jpg
+
+SELECT ROUND(AVG(POPULATION)) FROM CITY;
