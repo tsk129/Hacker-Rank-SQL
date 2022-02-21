e3d4a860aa6914c765998ce7b5a17a2c5d75ce47
SAMPATH <59731374+tsk129@users.noreply.github.com>
2022-02-21 23:03:40 +0530

    Create Weather Observation Station 14.sql

---
+++ Weather Observation Station 14.sql
@@ -0,0 +1,11 @@
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than . Truncate your answer to  decimal places.
+
+Input Format
+
+The STATION table is described as follows:
+
+Station.jpg
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+SELECT TRUNCATE(MAX(VALID),4) FROM (SELECT LAT_N AS VALID FROM STATION WHERE LAT_N<137.2345) VALID; 
